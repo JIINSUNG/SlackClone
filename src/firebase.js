ab92d49e88c65f7e2e9c13b8c92f05b9c7415b37
Jiinsung <crm03008@gmail.com>
2023-01-03 16:21:26 +0900

    ReactJS 셋팅, 라우터 구축

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC3VjeFVlVg6VjD9F_eSKl1Dzyhl613oS8",
+    authDomain: "slackclone-2cdcb.firebaseapp.com",
+    projectId: "slackclone-2cdcb",
+    storageBucket: "slackclone-2cdcb.appspot.com",
+    messagingSenderId: "528298926860",
+    appId: "1:528298926860:web:9fdae4929dcc920bf31f29",
+    measurementId: "G-VPCXZW2RX0"
+  };
