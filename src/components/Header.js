c91f1030529f8429c79ca904692047b4aa790cdc
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2023-01-05 17:21:12 +0900

    웹페이지 상단부 헤더

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import { Avatar } from "@material-ui/core";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 function Header() {
